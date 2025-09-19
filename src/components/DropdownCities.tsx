b95470166e854d537d070789920282ff091f063f
Dan G <hospedajesvnz@gmail.com>
2025-09-18 18:53:22 -0700

    deploy fix

--- src/components/DropdownCities.tsx
+++ src/components/DropdownCities.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import PropertyCard from "../components/PropertyCard";
+import PropertyCard from "./PropertyCard";
 
 const cities = [
   { label: "Valencia, Carabobo", value: "Valencia, Carabobo" },
